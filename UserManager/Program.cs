01d3c2cbff2d9935f9b0eba1e9a68ca528f1b4db
ijeoma-tech <ijeoma@storesmart.africa>
2024-10-10 10:04:55 +0100

    Added Database connectionstring

--- UserManager/Program.cs
+++ UserManager/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UserManager.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseSqlServer(builder.Configuration.GetConnectionString("UserManagerDbCon")));
 
 var app = builder.Build();
 
